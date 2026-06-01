b3c306a24cba71811751fb3de5050df41c677c4c
sasan safaei <sasan.safaei@ica.de>
2026-06-01 16:27:13 +0200

    modify find .cfg folder on STM32ProgFunc

--- TM_V2/Source/STM32ProgFunc/main.cpp
+++ TM_V2/Source/STM32ProgFunc/main.cpp
@@ -4,14 +4,32 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <limits.h>
 
 
 int main(int argc, char* argv[]) {
-    //STM32G030F6_Class stm32("./OpenOCD_CFG/raspberrypi2-native.cfg", "./OpenOCD_CFG/stm32g0x.cfg", 
-    //                            0x08000000, 50, "/dev/ttyAMA1", 9600);
-    
-    STM32G030F6_Class stm32("interface/raspberrypi2-native.cfg", "target/stm32g0x.cfg", 
+    // Resolve paths relative to the executable location so the program
+    // can be started from any working directory.
+    char exePath[PATH_MAX] = {0};
+    std::string exeDir = ".";
+    ssize_t len = readlink("/proc/self/exe", exePath, sizeof(exePath) - 1);
+    if (len != -1) {
+        exePath[len] = '\0';
+        std::string fullPath(exePath);
+        auto pos = fullPath.find_last_of('/');
+        if (pos != std::string::npos)
+            exeDir = fullPath.substr(0, pos);
+    }
+
+    std::string cfg_interface = exeDir + "/OpenOCD_CFG/raspberrypi2-native.cfg";
+    std::string cfg_target = exeDir + "/OpenOCD_CFG/stm32g0x.cfg";
+
+    STM32G030F6_Class stm32(cfg_interface.c_str(), cfg_target.c_str(), 
                                 0x08000000, 50, "/dev/ttyAMA1", 9600);
+    
+    //STM32G030F6_Class stm32("interface/raspberrypi2-native.cfg", "target/stm32g0x.cfg", 
+    //                            0x08000000, 50, "/dev/ttyAMA1", 9600);
     //stm32.Flash_Func({"--ob"});
     //stm32.Flash_Func({"--reset"});    
     //return stm32.Flash_Func(argc, argv);
