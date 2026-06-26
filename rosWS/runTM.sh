5b2d3fa3807c3f2f7addae6e8b87bb78bccf5953
sasan safaei <sasan.safaei@ica.de>
2026-06-26 15:43:04 +0200

    modify for  ROS_DOMAIN_ID

--- rosWS/runTM.sh
+++ rosWS/runTM.sh
@@ -32,9 +32,19 @@ setup_gpio 19 out 1
 export TM_WORKSPACE="$PROJECT_ROOT/TM_WorkSpace/"
 
 export RCUTILS_CONSOLE_OUTPUT_FORMAT="[{severity}] [{name}]: {message}"
+# Avoid loading ~/.local Python packages that can conflict with ROS/apt packages.
+export PYTHONNOUSERSITE=1
 source /opt/ros/humble/setup.bash
 source install/setup.bash
+
+#***************************************
 export ROS_DOMAIN_ID=1
+#***************************************
+#export ROS_DOMAIN_ID=${ROS_DOMAIN_ID:-1}
+
+# Set StoreFolder in config based on ROS_DOMAIN_ID
+STORE_FOLDER="./TestMachine$(printf '%03d' $ROS_DOMAIN_ID)"
+sed -i "s|StoreFolder = .*|StoreFolder = $STORE_FOLDER|" "$TM_WORKSPACE/config.cfg"
 #QT_QPA_PLATFORM=linuxfb ros2 run qt_gui_node_pkg gui_node tm_node_pkg tm_node
 
 # Start tm_node in the background
