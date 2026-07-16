233ff48642b06bf32015729c00cedb5c80fedf66
sasan safaei <sasan.safaei@ica.de>
2026-07-16 15:19:34 +0200

    Modify to prevent double running of nodes.

--- rosWS/runTM.sh
+++ rosWS/runTM.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Forward script logs to journald when started by systemd.
+if command -v systemd-cat >/dev/null 2>&1; then
+	exec > >(systemd-cat -t runTM -p info) 2> >(systemd-cat -t runTM -p err)
+fi
+
+echo "Starting runTM.sh"
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 cd "$SCRIPT_DIR"
@@ -48,6 +55,29 @@ sed -i "s|StoreFolder = .*|StoreFolder = $STORE_FOLDER|" "$TM_WORKSPACE/config.c
 #QT_QPA_PLATFORM=linuxfb ros2 run qt_gui_node_pkg gui_node tm_node_pkg tm_node
 
 # Start tm_node in the background
-ros2 run tm_node_pkg tm_node &
+#ros2 run tm_node_pkg tm_node &
 # Start gui_node with the QT framebuffer environment
-QT_QPA_PLATFORM=linuxfb ros2 run qt_gui_node_pkg gui_node
+#QT_QPA_PLATFORM=linuxfb ros2 run qt_gui_node_pkg gui_node
+
+
+
+#ros2 run tm_node_pkg tm_node >> /tmp/tm_node.log 2>&1 &
+if ! pgrep -f "tm_node_pkg tm_node" > /dev/null; then
+    ros2 run tm_node_pkg tm_node &
+else
+    echo "tm_node already running"
+fi
+
+if ! pgrep -f "qt_gui_node_pkg gui_node" > /dev/null; then
+	while true; do
+		echo "$(date): Starting gui_node" #>> /tmp/gui_node.log
+		QT_QPA_PLATFORM=linuxfb ros2 run qt_gui_node_pkg gui_node #\
+			#>> /tmp/gui_node.log 2>&1
+
+		echo "$(date): gui_node stopped. Restarting..." #>> /tmp/gui_node.log
+		sleep 2
+    done
+else
+    echo "gui_node already running"
+fi
+
